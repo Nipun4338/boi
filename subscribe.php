634bd28bbc9670bd210801937fe873ae645bddf5
Nipun4338 <49658560+Nipun4338@users.noreply.github.com>
2021-04-19 23:52:01 +0600

    update sign up

--- subscribe.php
+++ subscribe.php
@@ -100,7 +100,7 @@ $datetime = '';
 	<title>Subscribe | বই</title>
 	<meta charset="UTF-8">
 
-	<meta name="viewport" content="width-device-width, initial scale = 1.0">
+	<meta name="viewport" content="width=device-width, initial-scale = 1.0">
 
 	<script src = "https://code.jquery.com/jquery-2.1.3.min.js"></script>
 
