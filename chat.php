69d6bbed54d46a573ea5a8169fcff6bf49e387fe
Nipun4338 <49658560+Nipun4338@users.noreply.github.com>
2021-05-04 19:06:38 +0600

    Update chat.php

--- chat.php
+++ chat.php
@@ -190,6 +190,7 @@ include('database/dbconfig.php');
  		</div>
  		<div class="col-9">
 			<div class="content-section">
+<div id="display_message"></div>
 </div>
 <form class="form-container" id="message_form" method="POST">
 <div class="form-group">
@@ -199,7 +200,7 @@ include('database/dbconfig.php');
 <input type="submit" name="submit" id="submit" class="btn btn-primary" value="SEND"  style="float:right">
 </div>
 </form>
-<div id="display_message"></div>
+
 <script>
 $(document).ready(function(){
 
