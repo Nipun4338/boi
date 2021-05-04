fa90e777dca82db95f2e44539854d2fb227e16af
Nipun4338 <49658560+Nipun4338@users.noreply.github.com>
2021-05-04 20:11:16 +0600

    Update fetch_message.php

--- fetch_message.php
+++ fetch_message.php
@@ -28,15 +28,13 @@ else {
    if ($row["type"]=="receive") {
      $output .='<div  class="container1" style="background:#fff;">
        <h6 style="font-weight:bold;color:#a08">'.$_SESSION["receive_name"].'</h6>
-     <p style="font-size:rfs-fluid-value(1.125rem);font-family:Helvetica">'.nl2br($row["message"]).'</p>
-     <span style="font-size:12px;font-family:Helvetica" class="time-right">'.date("M j, Y g:i A", strtotime($row["date"])).'</span>
+     <p style="font-size:rfs-fluid-value(1.125rem);font-family:Helvetica">'.$row["message"].'</p>
      </div>';
 
    }
    else {
      $output .='<div  class="container2" style="background:#fff;" >
-     <p style="font-size:rfs-fluid-value(1.125rem);font-family:Helvetica">'.nl2br($row["message"]).'</p>
-     <span style="font-size:12px;font-family:Helvetica" class="time-right">'.date("M j, Y g:i A", strtotime($row["date"])).'</span>
+     <p style="font-size:rfs-fluid-value(1.125rem);font-family:Helvetica">'.$row["message"].'</p>
      </div>';
    }
  }
