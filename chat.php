95dff2165731f27ba3e38679267b09563043ac09
Nipun4338 <49658560+Nipun4338@users.noreply.github.com>
2021-05-04 19:00:59 +0600

    Update chat.php

--- chat.php
+++ chat.php
@@ -234,12 +234,32 @@ function load_message()
 	dataType:'json',
 	success:function(data)
 	{
+		<?php $query = "
+		INSERT INTO comments
+		(comment)
+		VALUES ('ok')
+		";
+		$statement = $connection->prepare($query);
+		$statement->execute(); ?>
 	 $('#display_message').html(data);
 	 window.scrollTo(0, document.body.scrollHeight);
 	}
  })
 }
 
+function load_message1()
+{
+ $.ajax({
+	url:"fetch_message.php",
+	method:"POST",
+	data:{ running : "yes" },
+	dataType:'json',
+	success:function(data)
+	{
+	 $('#display_message').html(data);
+	}
+ })
+}
 
 });
 </script>
