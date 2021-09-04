13a39420aea38ea6bb9eee0e9b820eb68e7f96bd
Nipun Paul <49658560+Nipun4338@users.noreply.github.com>
2021-09-05 00:46:39 +0600

    Update mail_test.php

--- mail_test.php
+++ mail_test.php
@@ -8,5 +8,28 @@ $mail=new PHPMailer(true);
          $mail->isSMTP();
          $mail->Host='smtp.gmail.com';
          $mail->SMTPAuth=true;
-         
+         $mail->Username='boi.yourbook@gmail.com';
+         $mail->Password='boi@boi.';
+         $mail->SMTPSecure=PHPMailer::ENCRYPTION_STARTTLS;
+         $mail->Port=587;
+         $mail->setFrom('boi.yourbook@gmail.com', 'Boi');
+         $mail->addAddress("nipun4337@gmail.com");
+         $mail->isHTML(true);
+         $mail->Subject="Registration Verification of Boi";
+         $mail->Body= '
+
+          <p>Thanks for signing up!</p>
+          <p>Your account has been created, you can login with the following credentials after you have activated your account by pressing the url below.</p>
+
+
+          <p>------------------------</p>
+          <p>Name: </p>
+          <p>------------------------</p>
+
+          <p>Please click this link to activate your account:</p>
+          <p><a href="http://boi-yourbook.herokuapp.com/">Click to Verify</a></p>
+
+          '; // Our message above including the link
+
+          
  ?>
