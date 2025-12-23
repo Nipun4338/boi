402da3192c8f1ceac502d59854fa1cb970c24950
LAPTOP-70C060U7\USER <nipun4338@gmail.com>
2025-12-23 11:20:27 +0600

    Update db config

--- includes/config/dbconfig.php
+++ includes/config/dbconfig.php
@@ -6,11 +6,23 @@ $server_name = getenv('DB_HOST') ?: "localhost";
 $db_username = getenv('DB_USER') ?: "root";
 $db_password = getenv('DB_PASS') ?: "";
 $db_name     = getenv('DB_NAME') ?: "boi_db";
+$db_port     = getenv('DB_PORT') ?: "3306";
 
-$connection = mysqli_connect($server_name, $db_username, $db_password, $db_name);
+// Create connection
+$connection = mysqli_init();
+
+// Aiven and some other cloud providers require SSL
+// If we are on Vercel (remote), we should use the port and try to connect
+if (getenv('DB_HOST')) {
+    $connection = mysqli_connect($server_name, $db_username, $db_password, $db_name, $db_port);
+} else {
+    // Local XAMPP
+    $connection = mysqli_connect($server_name, $db_username, $db_password, $db_name);
+}
+
 $link = $connection; // Standardizing on one connection variable
 
 if (!$connection) {
-    die("Connection failed: " . mysqli_connect_error());
+    die("Database Connection failed: " . mysqli_connect_error());
 }
 ?>
